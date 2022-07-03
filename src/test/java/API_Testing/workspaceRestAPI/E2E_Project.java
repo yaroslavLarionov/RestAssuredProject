b82a1baad938bd2da58d9e301dbf6ce6fbc6248b
yaroslavLarionov <yarillo@yahoo.com>
2022-07-03 12:00:52 -0400

    added more end to end tests

--- src/test/java/API_Testing/workspaceRestAPI/E2E_Project.java
+++ src/test/java/API_Testing/workspaceRestAPI/E2E_Project.java
@@ -10,10 +10,19 @@ import org.testng.annotations.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.apache.http.HttpStatus.SC_NO_CONTENT;
 import static org.apache.http.HttpStatus.SC_OK;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class E2E_Project {
     public String path;
+    String id;
+    String userId;
+    String projectID;
+    Map<String, String> variables;
+    Response response;
+
 
     //What is a TestNG annotation that allows us to run a Test Before each method
     @BeforeTest
@@ -39,12 +48,20 @@ public class E2E_Project {
 
     //TODO write a test for API endpoint member-of
     @Test
-    public void verifyToken() {
+    public void memberOf() {
         String memberOf = "/workspaces/member-of";
-        Response response = RestAssured.given().header("authorization", setupLogInAndToken()).when().get(memberOf).then().log().all().extract().response();
+        response = RestAssured.given().header("authorization", setupLogInAndToken()).when().get(memberOf).then().log().all().extract().response();
         //Verify status code
         Assert.assertEquals(SC_OK, response.statusCode());
         Assert.assertEquals("Default", response.jsonPath().getString("name[0]"));
+
+        //Save the id so it could be used in other requests
+        id = response.jsonPath().get("id[0]");
+        //Save the userId so it can be used in other requests
+        userId = response.jsonPath().get("userId[0]");
+        variables = new HashMap<>();
+        variables.put("id", id);
+        variables.put("userId", userId);
     }
 
     //TODO add tests for ID, userID, Description
@@ -71,5 +88,46 @@ public class E2E_Project {
         //Verify description
         Assert.assertEquals("", response.jsonPath().getString("description[0]"));
     }
+
+    @Test(dependsOnMethods = {"memberOf"})
+    public void createProject() {
+        String requestBody = "{\"id\":\"\",\"created\":\"2021-03-11T06:15:20.845Z\",\"lastModified\":\"2021-03-11T06:15:20.845Z\",\"userId\":\"" +
+                            variables.get("userId") + "\",\"workspaceId\":\"" + variables.get("id") +
+                             "\",\"name\":\"testing22\",\"description\":\"testing\",\"type\":\"DESIGN\",\"tags\":[]}";
+
+        response = RestAssured.given().headers("Content-type", ContentType.JSON).header("authorization", setupLogInAndToken()).and().body(requestBody).when().post("/design/projects").then()
+                .extract().response();
+
+        Assert.assertEquals("testing22", response.jsonPath().getString("name"));
+        // Using hamcrest matchers validation
+        assertThat(response.jsonPath().getString("name"), is("testing22"));
+        Assert.assertEquals(id, response.jsonPath().getString("workspaceId"));
+        Assert.assertEquals(userId, response.jsonPath().getString("userId"));
+
+        //Store id on a variable for future use
+        projectID = response.jsonPath().get("id");
+    }
+
+    @Test(dependsOnMethods = {"memberOf", "createProject"}, description = "we need those 2 methods to be able to update the project")
+    public void updateProject() {
+        String requestBody1 = "{\"created\":1615443320845,\"description\":\"TLAupdate\",\"id\":\"" + projectID +
+                               "\",\"lastModified\":1629860121757,\"name\":\"TLA accounting\",\"tags\":[],\"type\":\"DESIGN\",\"userId\":\"" +
+                               variables.get("userID") + "\",\"workspaceId\":\"" + variables.get("id") + "\"}";
+
+        response = RestAssured.given().headers("Content-type", ContentType.JSON).header("authorization", setupLogInAndToken()).and().body(requestBody1)
+                   .when().put("/design/projects/" + projectID).then().extract().response();
+        System.out.println(response.prettyPrint());
+
+        //TODO do assertions for id, name, type, userId, workspaceId, status code, Content-type
+    }
+
+    @Test(dependsOnMethods = {"memberOf", "createProject", "updateProject"})
+    public void deleteProject() {
+        response = RestAssured.given().header("authorization", setupLogInAndToken()).when().delete("design/projects/" + projectID).then()
+                .extract().response();
+        Assert.assertEquals(SC_NO_CONTENT, response.statusCode());
+    }
+
+
 
 }
